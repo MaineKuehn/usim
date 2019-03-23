aa62065690011379cef2e171de7ad31f9a4952bf
Max Fischer <maxfischer2781@gmail.com>
2019-03-23 18:52:35 +0100

    added metadata

---
+++ usim/__about__.py
@@ -0,0 +1,14 @@
+"""
+==========================
+``usim`` - Simply Simulate
+==========================
+"""
+__title__ = 'usim'
+__summary__ = 'Lightweight Simulation Framework'
+__url__ = 'https://github.com/MaineKuehn/usim'
+
+__version__ = '0.0.1'
+__author__ = 'Eileen Kuehn, Max Fischer'
+__email__ = 'mainekuehn@gmail.com'
+__copyright__ = '2019 %s' % __author__
+__keywords__ = 'simulation simpy event loop async coroutine'
