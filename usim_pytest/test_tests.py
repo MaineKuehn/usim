e11df0f77288a3936cc98ab815303dcb0f74dea1
Max Fischer <maxfischer2781@gmail.com>
2019-03-24 09:19:38 +0100

    added tests for test setup

---
+++ usim_pytest/test_tests.py
@@ -0,0 +1,25 @@
+import pytest
+
+from usim import Scope
+
+from .utility import via_usim
+
+
+async def a_raise(exc: BaseException):
+    """Raise an exception in an ``await`` or ``scope.do`` context"""
+    raise exc
+
+
+class TestTests:
+    """Test propagation of test exceptions/signals"""
+    @pytest.mark.xfail(strict=True)
+    @via_usim
+    async def test_run(self):
+        """Test failure in a top-level activity"""
+        assert False
+
+    @pytest.mark.xfail(raises=IndexError, strict=True)
+    @via_usim
+    async def test_scoped(self):
+        async with Scope() as scope:
+            scope.do(a_raise(KeyError()))
