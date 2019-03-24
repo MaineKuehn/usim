65f98114b61f151e86ca2c6419f5cc3e67c2b631
Max Fischer <maxfischer2781@gmail.com>
2019-03-24 09:20:42 +0100

    via_usim decorator rethrows exceptions from pytest

--- usim_pytest/utility.py
+++ usim_pytest/utility.py
@@ -2,6 +2,7 @@ from typing import Callable, Coroutine
 from functools import wraps
 
 from usim import run
+from usim._core.loop import ActivityError
 
 
 def via_usim(test_case: Callable[..., Coroutine]):
@@ -19,6 +20,13 @@ def via_usim(test_case: Callable[..., Coroutine]):
     """
     @wraps(test_case)
     def run_test(*args, **kwargs):
+        # pytest currently ignores __tracebackhide__ if we re-raise
+        # https://github.com/pytest-dev/pytest/issues/1904
         __tracebackhide__ = True
-        return run(test_case(*args, **kwargs))
+        # >>> This is not the frame you are looking for. Do read on. <<<
+        try:
+            return run(test_case(*args, **kwargs))
+        except ActivityError as err:
+            # unwrap any exceptions
+            raise err.__cause__
     return run_test
