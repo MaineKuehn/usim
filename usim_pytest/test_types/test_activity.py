8e8eaabd0392f04899472485d10eb1545ab04630
Max Fischer <maxfischer2781@gmail.com>
2019-03-26 11:10:48 +0100

    added activity status tests

--- usim_pytest/test_types/test_activity.py
+++ usim_pytest/test_types/test_activity.py
@@ -1,13 +1,17 @@
-from usim import time, Scope
+from usim import time, Scope, ActivityState, instant
 
 from ..utility import via_usim
+
+
+async def sleep(duration):
+    await (time + duration)
 
 
 class TestExecution:
     @via_usim
     async def test_await(self):
         async with Scope() as scope:
-            activity = scope.do(time + 20)
+            activity = scope.do(sleep(20))
             assert time.now == 0
             # await inside scope
             await activity
@@ -16,3 +20,41 @@ class TestExecution:
         await activity
         assert time.now == 20
 
+    @via_usim
+    async def test_state_success(self):
+        async with Scope() as scope:
+            activity = scope.do(sleep(20))
+            assert activity.status == ActivityState.CREATED
+            await instant
+            assert activity.status == ActivityState.RUNNING
+            await activity
+            assert activity.status == ActivityState.SUCCESS
+            assert activity.status & ActivityState.FINISHED
+
+    @via_usim
+    async def test_state_cancel_created(self):
+        async with Scope() as scope:
+            activity = scope.do(sleep(20))
+            assert activity.status == ActivityState.CREATED
+            activity.cancel()
+            # early cancellation does not run
+            assert activity.status == ActivityState.CANCELLED
+            await instant
+            assert activity.status == ActivityState.CANCELLED
+            await activity
+            assert activity.status == ActivityState.CANCELLED
+            assert activity.status & ActivityState.FINISHED
+
+    @via_usim
+    async def test_state_cancel_running(self):
+        async with Scope() as scope:
+            activity = scope.do(sleep(20))
+            assert activity.status == ActivityState.CREATED
+            await instant
+            assert activity.status == ActivityState.RUNNING
+            activity.cancel()
+            # running cancellation is graceful
+            assert activity.status == ActivityState.RUNNING
+            await activity
+            assert activity.status == ActivityState.CANCELLED
+            assert activity.status & ActivityState.FINISHED
