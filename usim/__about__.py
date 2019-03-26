85294bc11e166336e829e2d8f4e0a5df9a635d1d
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-03-26 15:18:23 +0100

    set version to 0.1.0

--- usim/__about__.py
+++ usim/__about__.py
@@ -7,7 +7,7 @@ __title__ = 'usim'
 __summary__ = 'Lightweight Simulation Framework'
 __url__ = 'https://github.com/MaineKuehn/usim'
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __author__ = 'Eileen Kuehn, Max Fischer'
 __email__ = 'mainekuehn@gmail.com'
 __copyright__ = '2019 %s' % __author__
