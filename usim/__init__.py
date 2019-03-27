9804969415964456e6a7fe0be94ea1cf3d5b4e87
Max Fischer <maxfischer2781@gmail.com>
2019-03-27 16:16:45 +0100

    usim.run accepts a maximum simulation time

--- usim/__init__.py
+++ usim/__init__.py
@@ -18,13 +18,20 @@ __all__ = [
 
 # User entry point
 ##################
-def run(*activities: Coroutine, start: float = 0):
+def run(*activities: Coroutine, start: float = 0, till: float = None):
     """
     Run a simulation from the initial ``activities`` at time ``start`` until completion
 
     :param activities: initial activities of the simulation
     :param start: initial time of the simulation
+    :param till: time at which to terminate the simulation
     """
+    if till is not None:
+        async def root(_activities=activities, _till=till):
+            async with until(time == _till) as scope:
+                for activity in _activities:
+                    scope.do(activity)
+        activities = root(_activities=activities, _till=till),
     loop = _Loop(*activities, start=start)
     loop.run()
 
