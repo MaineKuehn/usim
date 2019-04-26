17a43deb832f5615241430586c258ea1288d9dda
Max Fischer <maxfischer2781@gmail.com>
2019-04-26 19:02:51 +0200

    added negation tests for chaining

--- usim_pytest/test_types/test_condition.py
+++ usim_pytest/test_types/test_condition.py
@@ -1,24 +1,29 @@
-from usim import Scope, Flag
+from usim import Flag
 
 from ..utility import via_usim
 
 
-class TestCondition:
+class TestChainedCondition:
     @via_usim
     async def test_chain_and(self):
         a, b, c = Flag(), Flag(), Flag()
         assert not a and not b and not c
         and_chain = a & b & c
         assert not and_chain
-        async with Scope() as scope:
-            wait_chain = scope.do(and_chain)
-            await a.set()
-            assert not and_chain
-            await b.set()
-            assert not and_chain
-            await c.set()
-            assert and_chain
-            await wait_chain
+        assert ~and_chain
+        assert await (~and_chain)
+        await a.set()
+        assert not and_chain
+        assert ~and_chain
+        assert await (~and_chain)
+        await b.set()
+        assert not and_chain
+        assert ~and_chain
+        assert await (~and_chain)
+        await c.set()
+        assert and_chain
+        assert not ~and_chain
+        assert await and_chain
 
     @via_usim
     async def test_chain_or(self):
@@ -26,12 +31,37 @@ class TestCondition:
         assert not a or not b or not c
         or_chain = a | b | c
         assert not or_chain
-        async with Scope() as scope:
-            wait_chain = scope.do(or_chain)
-            await a.set()
-            assert or_chain
-            await b.set()
-            assert or_chain
-            await c.set()
-            assert or_chain
-            await wait_chain
+        assert ~or_chain
+        assert await (~or_chain)
+        await a.set()
+        assert or_chain
+        assert not ~or_chain
+        assert await or_chain
+        await b.set()
+        assert or_chain
+        assert not ~or_chain
+        assert await or_chain
+        await c.set()
+        assert or_chain
+        assert not ~or_chain
+        assert await or_chain
+
+    @via_usim
+    async def test_chain_long(self):
+        a, b, c, d, e, f = Flag(), Flag(), Flag(), Flag(), Flag(), Flag()
+        chain = a & (b & c) & d | (e | f)
+        assert not chain
+        assert ~chain
+        assert await (~chain)
+        await d.set()
+        assert not chain
+        assert ~chain
+        assert await (~chain)
+        await c.set()
+        assert not chain
+        assert ~chain
+        assert await (~chain)
+        await e.set()
+        assert chain
+        assert not ~chain
+        assert await chain
