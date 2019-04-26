bcd3b27682294993dd6ab6327fb681b1f686330e
Max Fischer <maxfischer2781@gmail.com>
2019-04-26 13:30:28 +0200

    added contribution requirements, travis enforces linter

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
         extras_require={
             'docs': ["sphinx", "sphinx_rtd_theme"],
             'test': TESTS_REQUIRE,
+            'contrib': ['flake8', 'flake8-bugbear'] + TESTS_REQUIRE,
         },
         # metadata for package search
         license='MIT',
