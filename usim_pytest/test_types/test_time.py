24e3437e5b8240fb59457ccd1ab913302fa69fdd
Max Fischer <maxfischer2781@gmail.com>
2019-04-30 14:28:53 +0200

    added tests for time == date, time >= date and time <= date

--- usim_pytest/test_types/test_time.py
+++ usim_pytest/test_types/test_time.py
@@ -1,6 +1,6 @@
 import pytest
 
-from usim import time
+from usim import time, until
 
 from ..utility import via_usim
 
@@ -10,6 +10,8 @@ class TestTime:
     async def test_misuse(self):
         with pytest.raises(TypeError):
             await time
+        with pytest.raises(TypeError):
+            await (time <= 100)
 
     @via_usim
     async def test_delay(self):
@@ -18,3 +20,35 @@ class TestTime:
             assert start + (delay * seq) == time.now
             await (time + delay)
         assert start + (delay * 5) == time.now
+
+    @via_usim
+    async def test_moment(self):
+        start, delay = time.now, 20
+        for seq in range(5):
+            await (time == start + seq * delay)
+            assert start + (delay * seq) == time.now
+        assert start + (delay * 4) == time.now
+
+    @via_usim
+    async def test_previous_moment(self):
+        start, delay = time.now, 20
+        async with until(time == start + delay):
+            await (time == start - 5)  # await moment in the past
+            assert False, "Moment in the past should never pass"
+        assert start + delay == time.now
+
+    @via_usim
+    async def test_after(self):
+        start, delay = time.now, 20
+        for seq in range(5):
+            await (time >= start + seq * delay)
+            assert start + (delay * seq) == time.now
+        assert start + (delay * 4) == time.now
+
+    @via_usim
+    async def test_previous_after(self):
+        start, delay = time.now, 20
+        async with until(time == start + delay):
+            await (time >= start - 5)  # await moment in the past
+            assert True, "After in the past should always pass"
+        assert start == time.now
