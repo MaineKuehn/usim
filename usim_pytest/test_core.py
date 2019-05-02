1c7526c879541097b780d45990ec7dc379acd7a2
Max Fischer <maxfischer2781@gmail.com>
2019-05-02 20:01:27 +0200

    added test for placeholder loop being reset after a simulation

--- usim_pytest/test_core.py
+++ usim_pytest/test_core.py
@@ -1,6 +1,6 @@
 import pytest
 
-from usim import time
+from usim import time, run
 from usim._core.loop import __LOOP_STATE__, Loop
 
 
@@ -11,3 +11,7 @@ class TestCore:
         for field in Loop.__slots__:
             with pytest.raises(RuntimeError):
                 getattr(__LOOP_STATE__.LOOP, field)
+
+    def test_after_sim(self):
+        run()
+        self.test_no_sim()
