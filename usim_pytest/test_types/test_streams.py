2c5176eb44e2193a619f828097b5e2bf82116195
Max Fischer <maxfischer2781@gmail.com>
2019-05-02 19:15:19 +0200

    stream test covers both queue and channel

--- usim_pytest/test_types/test_streams.py
+++ usim_pytest/test_types/test_streams.py
@@ -1,65 +1,77 @@
 import pytest
+from typing import Type
 
 from usim import time, Scope
-from usim.basics import Queue, StreamClosed
+from usim.basics import Queue, Channel, StreamClosed
+from usim.typing import Stream
 
 from ..utility import via_usim, turnstamp
 
 
-class TestQueue:
+class Base1to1Stream:
+    stream_type = None  # type: Type[Stream]
+
     @via_usim
     async def test_close(self):
-        queue = Queue()
-        await queue.close()
+        stream = self.stream_type()
+        await stream.close()
         with pytest.raises(StreamClosed):
-            await queue.put(None)
+            await stream.put(None)
         with pytest.raises(StreamClosed):
-            await queue
+            await stream
         start = turnstamp()
-        await queue.close()
+        await stream.close()
         end = turnstamp()
         assert end > start
 
     @via_usim
     async def test_put_get(self):
-        queue = Queue()
+        stream = self.stream_type()
 
         async def fill(*values, delay: float = 5):
             for value in values:
-                await queue.put(value)
+                await stream.put(value)
                 await (time + delay)
-            await queue.close()
+            await stream.close()
 
         async def read():
             values = []
             try:
                 while True:
-                    values.append(await queue)
+                    values.append(await stream)
             except StreamClosed:
                 return values
 
         async with Scope() as scope:
-            scope.do(fill(*range(20)))
             receiver = scope.do(read())
+            scope.do(fill(*range(20)))
         assert (await receiver) == list(range(20))
 
     @via_usim
     async def test_put_stream(self):
-        queue = Queue()
+        stream = self.stream_type()
 
         async def fill(*values, delay: float = 5):
             for value in values:
-                await queue.put(value)
+                await stream.put(value)
                 await (time + delay)
-            await queue.close()
+            await stream.close()
 
         async def read():
             values = []
-            async for value in queue:
+            async for value in stream:
                 values.append(value)
             return values
 
         async with Scope() as scope:
-            scope.do(fill(*range(20)))
             receiver = scope.do(read())
+            scope.do(fill(*range(20)))
         assert (await receiver) == list(range(20))
+
+
+class Test1to1Queue(Base1to1Stream):
+    stream_type = Queue
+
+
+class Test1to1Channel(Base1to1Stream):
+    stream_type = Channel
