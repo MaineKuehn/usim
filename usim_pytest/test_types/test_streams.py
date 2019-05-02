3077320b80b4bb9298d30a84e2c49a145a37f8f3
Max Fischer <maxfischer2781@gmail.com>
2019-05-02 11:48:19 +0200

    basic tests for queue

---
+++ usim_pytest/test_types/test_streams.py
@@ -0,0 +1,50 @@
+from usim import time, Scope
+from usim.basics import Queue, StreamClosed
+
+from ..utility import via_usim
+
+
+class TestQueue:
+    @via_usim
+    async def test_put_get(self):
+        queue = Queue()
+
+        async def fill(*values, delay: float = 5):
+            for value in values:
+                await queue.put(value)
+                await (time + delay)
+            await queue.close()
+
+        async def read():
+            values = []
+            try:
+                while True:
+                    values.append(await queue)
+            except StreamClosed:
+                return values
+
+        async with Scope() as scope:
+            scope.do(fill(*range(20)))
+            receiver = scope.do(read())
+        assert (await receiver) == list(range(20))
+
+    @via_usim
+    async def test_put_stream(self):
+        queue = Queue()
+
+        async def fill(*values, delay: float = 5):
+            for value in values:
+                await queue.put(value)
+                await (time + delay)
+            await queue.close()
+
+        async def read():
+            values = []
+            async for value in queue:
+                values.append(value)
+            return values
+
+        async with Scope() as scope:
+            scope.do(fill(*range(20)))
+            receiver = scope.do(read())
+        assert (await receiver) == list(range(20))
