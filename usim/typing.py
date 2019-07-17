38d6bb74687f87d6e82e3b1f26feae86c4567db1
Max Fischer <maxfischer2781@gmail.com>
2019-07-17 21:34:14 +0200

    exposing BorrowedResources and ResourceLevels for type hinting

--- usim/typing.py
+++ usim/typing.py
@@ -2,10 +2,12 @@ from ._primitives.notification import Notification
 from ._primitives.condition import Condition
 from ._primitives.task import Task
 from ._basics.streams import Stream, StreamAsyncIterator
+from ._basics.resource import BorrowedResources, ResourceLevels
 
 
 __all__ = [
     'Notification', 'Condition',
     'Stream', 'StreamAsyncIterator',
     'Task',
+    'BorrowedResources', 'ResourceLevels'
 ]
