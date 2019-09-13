d5765fab4f9a5454dc70eb7b016c1b93274bc390
Max Fischer <maxfischer2781@gmail.com>
2019-09-13 16:19:06 +0200

    added documentation

--- usim/py/resources/base.py
+++ usim/py/resources/base.py
@@ -4,6 +4,8 @@ from itertools import takewhile
 from ..core import Environment
 from ..events import Event
 
+__all__ = ['Put', 'Get', 'BaseResource']
+
 
 # Implementation Note
 # We take some liberty in interpreting SimPy's API. Several parts
@@ -16,6 +18,24 @@ T = TypeVar('T')
 
 
 class BaseRequest(Event[T]):
+    r"""
+    Base class for :py:class:`~.Put` and :py:class:`~.Get` events
+
+    A request is commonly created via
+    :py:meth:`~.BaseResource.put` or :py:meth:`~.BaseResource.get` and must
+    be ``yield``\ ed by a Process to wait until the request has been served.
+    If a request has not been served but is no longer desired, the process should
+    :py:class:`~.cancel` the request.
+
+    A request can be used as a context manager to automatically cancel it
+    at the end of the context. Note that the request is not automatically
+    waited on -- this must be done explicitly if desired.
+
+    .. code:: python3
+
+        with resource.get() as request:
+            yield request
+    """
     def __init__(self, resource: 'BaseResource'):
         super().__init__(resource._env)
         self.resource = resource
@@ -29,10 +49,22 @@ class BaseRequest(Event[T]):
         self.cancel()
 
     def cancel(self):
+        """
+        Cancel the request
+
+        Requests should be cancelled when they have not been triggered but
+        are no longer needed. This may happen when a process is interrupted
+        while waiting for a request.
+
+
+        Cancelling is idempotent: if the request has already been triggered or canceled,
+        :py:meth:`~.cancel` can still be called without error.
+        """
         raise NotImplementedError
 
 
 class Put(BaseRequest[T]):
+    """Request to put content into a resource"""
     def __init__(self, resource: 'BaseResource'):
         super().__init__(resource)
         # Enqueue the event to be processed...
@@ -48,6 +80,7 @@ class Put(BaseRequest[T]):
 
 
 class Get(BaseRequest[T]):
+    """Request to get content out of a resource"""
     def __init__(self, resource: 'BaseResource'):
         super().__init__(resource)
         # Enqueue the event to be processed...
@@ -63,6 +96,46 @@ class Get(BaseRequest[T]):
 
 
 class BaseResource(Generic[T]):
+    """
+    Base class for all synchronised resources of :py:mod:`usim.py`
+
+    This type codifies the basic semantics of :py:mod:`usim.py` resources:
+    processes can :py:meth:`~.put`: content into the resource or :py:meth:`~.get`:
+    content out of the resource. Both actions return a :py:class:`~usim.py.events.Event`
+    which the process must ``yield``; once the event triggers, the content has been
+    successfully put into or gotten out of the resource.
+
+    .. code:: python3
+
+        def foo(env, resources: BaseResource):
+            print('Getting a resource at', env.time)
+            yield resources.get()
+            print('Got resource at', env.time)
+            yield env.timeout(2)
+            print('Returning a resource at', env.time)
+            yield resources.put()
+            print('Returned a resource at', env.time)
+
+    Subclasses should define their behaviour by implementing at least one of:
+
+    :py:attr:`~.BaseResource.PutQueue` or :py:attr:`~.BaseResource.GetQueue`
+        The types used to create the :py:attr:`~.put_queue` and :py:attr:`~.get_queue`.
+        These may, for example, customize priority of queued requests.
+
+    :py:meth:`~.put` or :py:meth:`~.get`
+        The methods used to create :py:class:`~.Put` and :py:class:`~.Get` events.
+        These may, for example, customize how much of a resource to handle at once.
+
+    :py:meth:`~._do_put` or :py:meth:`~._do_get`
+        The methods used to process :py:class:`~.Put` and :py:class:`~.Get` events.
+        These are an alternative to customizing :py:meth:`~.put` or :py:meth:`~.get`.
+
+    .. note::
+
+        This is *not* an Abstract Base Class.
+        Subclasses do not need to implement
+        :py:meth:`~.BaseResource._do_get` or :py:meth:`~.BaseResource._do_put`.
+    """
     #: The type used to create :py:attr:`~.put_queue`
     PutQueue: ClassVar[Type[Sequence]] = list
     #: The type used to create :py:attr:`~.get_queue`
@@ -77,9 +150,11 @@ class BaseResource(Generic[T]):
         self.get_queue = self.GetQueue()
 
     def put(self) -> Put[T]:
+        """Create a request to put content into the resource"""
         return Put(self)
 
     def get(self) -> Get[T]:
+        """Create a request to get content out of the resource"""
         return Get(self)
 
     def _trigger_put(self, get_event):
@@ -87,6 +162,9 @@ class BaseResource(Generic[T]):
         Trigger all possible put events
 
         Called when a ``Put`` event was created or a ``Get`` event triggered.
+        This trigger scheme handles a new request to an unconstested resource
+        (the ``Put`` succeeds immediately),
+        as well as a full one (the ``Get`` may serve a waiting ``Put``).
 
         :param get_event: ``Get`` event that was triggered or :py:const:`None`
         """
@@ -98,12 +176,16 @@ class BaseResource(Generic[T]):
         Trigger all possible put events
 
         Called when a ``Get`` event was created or a ``Put`` event triggered.
+        This trigger scheme handles a new request to an unconstested resource
+        (the ``Get`` succeeds immediately),
+        as well as a full one (the ``Put`` may serve a waiting ``Get``).
 
         :param put_event: ``Get`` event that was triggered or :py:const:`None`
         """
         triggered = list(takewhile(self._do_get, self.get_queue))
         self.get_queue = self.get_queue[len(triggered):]
 
+    # NOTE: Per the SimPy spec, these are **PUBLIC**
     def _do_get(self, get_event):
         raise NotImplementedError
 
