339c07afe143e6f3e84e47adb4ef90a3525ae181
Max Fischer <maxfischer2781@gmail.com>
2019-10-10 14:28:04 +0200

    actually removed usim.basics...

--- usim/basics.py
+++
@@ -1,8 +0,0 @@
-from ._basics.streams import Channel, Queue, StreamClosed
-from ._basics.tracked import Tracked
-from ._basics.resource import Capacities, Resources, ResourcesUnavailable
-
-__all__ = [
-    'Channel', 'Queue', 'StreamClosed',
-    'Tracked', 'Capacities', 'Resources', 'ResourcesUnavailable',
-]
