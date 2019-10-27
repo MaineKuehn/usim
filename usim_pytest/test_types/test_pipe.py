a285b64d8b07b48f325e38b190b5410199c75fd4
Max Fischer <maxfischer2781@gmail.com>
2019-10-27 14:19:40 +0100

    expanded Pipe tests

--- usim_pytest/test_types/test_pipe.py
+++ usim_pytest/test_types/test_pipe.py
@@ -3,6 +3,13 @@ from usim import time, Pipe, Scope
 from ..utility import via_usim
 
 
+async def aenumerate(aiterable):
+    count = 0
+    async for item in aiterable:
+        yield count, item
+        count += 1
+
+
 class TestPipe:
     @via_usim
     async def test_transfer_uncontested(self):
@@ -15,3 +22,35 @@ class TestPipe:
             scope.do(pipe.transfer(total=1, throughput=1))
             scope.do(pipe.transfer(total=1, throughput=1))
         assert (time == 3)
+
+    @via_usim
+    async def test_transfer_contested(self):
+        pipe = Pipe(throughput=2)
+        await pipe.transfer(total=2, throughput=4)
+        assert (time == 1)
+        await pipe.transfer(total=2, throughput=20)
+        assert (time == 2)
+        async with Scope() as scope:
+            scope.do(pipe.transfer(total=1, throughput=2))
+            scope.do(pipe.transfer(total=1, throughput=2))
+        assert (time == 3)
+
+    @via_usim
+    async def test_stream_uncontested(self):
+        pipe = Pipe(throughput=2)
+        async for idx, _ in aenumerate(pipe.stream(0.5, 1)):
+            assert (time == idx * 0.5)
+        async for idx, _ in aenumerate(pipe.stream(0.5, 2)):
+            assert (time == idx * 0.25)
+
+    @via_usim
+    async def test_stream_contested(self):
+        async def assert_stream(stream, delta):
+            async for idx, _ in aenumerate(stream):
+                assert (time == idx * delta)
+
+        pipe = Pipe(throughput=2)
+        async with Scope() as scope:
+            scope.do(assert_stream(pipe.stream(1, 10), 1))
+            scope.do(assert_stream(pipe.stream(1, 10), 1))
+        assert (time == 10)
