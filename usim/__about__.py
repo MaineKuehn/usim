0e90017fa0d3360f8fe3ae4d21ae9c16159e415c
Max Fischer <maxfischer2781@gmail.com>
2019-11-27 18:54:05 +0100

    bumped the version number

--- usim/__about__.py
+++ usim/__about__.py
@@ -32,7 +32,7 @@ __title__ = 'usim'
 __summary__ = 'Lightweight Simulation Framework'
 __url__ = 'https://github.com/MaineKuehn/usim'
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'Eileen Kuehn, Max Fischer'
 __email__ = 'mainekuehn@gmail.com'
 __copyright__ = '2019 %s' % __author__
