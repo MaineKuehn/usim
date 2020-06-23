e81645a663c573b35340774b5ef5e392302368b3
Eileen Kuehn <eileen.kuehn@kit.edu>
2020-06-23 12:15:59 +0200

    adapted unit tests for first and added new test cases for less available results

--- usim_pytest/test_flow/test_first.py
+++ usim_pytest/test_flow/test_first.py
@@ -23,9 +23,33 @@ async def test_collect_all(count):
     activities = [
         ping_pong(idx, delay=count - idx) for idx in range(count)
     ]
-    async for winner, expected in a.zip(first(*activities), reversed(range(count))):
+    async for winner, expected in a.zip(
+            first(*activities, count=None), reversed(range(count))):
         assert winner == expected
     assert (time == count)
+
+
+@pytest.mark.parametrize('count', (5, 12, 1))
+@via_usim
+async def test_collect_default(count):
+    activities = [
+        ping_pong(idx, delay=count - idx) for idx in range(count)
+    ]
+    async for winner in first(*activities):
+        assert winner == count - 1
+    assert (time == 1)
+
+
+@pytest.mark.parametrize('count', (1, 0))
+@via_usim
+async def test_less_available(count):
+    activities = [
+        ping_pong(idx, delay=count - idx) for idx in range(count)
+    ]
+    with pytest.raises(ValueError):
+        async for _ in first(*activities, count=3):
+            pass
+    assert (time == 0)
 
 
 @pytest.mark.parametrize('count', (5, 12, 1))
